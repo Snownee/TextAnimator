e3cf362f1cef16adba4becb5c094798012f770d3
Snownee <i@snownee.com>
2023-08-01 23:34:16 +0800

    1.20 port

--- src/main/java/snownee/textanimator/mixin/client/StringRenderOutputMixin.java
+++ src/main/java/snownee/textanimator/mixin/client/StringRenderOutputMixin.java
@@ -1,5 +1,6 @@
 package snownee.textanimator.mixin.client;
 
+import org.joml.Matrix4f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -9,7 +10,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import com.mojang.blaze3d.font.GlyphInfo;
 import com.mojang.blaze3d.vertex.VertexConsumer;
-import com.mojang.math.Matrix4f;
 
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.font.FontSet;
