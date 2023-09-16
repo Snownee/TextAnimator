9ef9ae57e6e95da39fc511f265854c4f4a9a2cd7
Snownee <i@snownee.com>
2023-09-17 04:58:45 +0800

    feat: display test extension point

--- src/main/java/snownee/textanimator/util/ClientProxy.java
+++ src/main/java/snownee/textanimator/util/ClientProxy.java
@@ -1,13 +1,17 @@
 package snownee.textanimator.util;
 
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.network.NetworkConstants;
 import snownee.textanimator.TextAnimatorClient;
 
 @Mod("textanimator")
 public class ClientProxy {
 
 	public ClientProxy() {
+		ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
 		if (FMLEnvironment.dist.isClient()) {
 			TextAnimatorClient.init();
 		}
