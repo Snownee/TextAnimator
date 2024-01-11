f52326e0d312a193a1d99695bf72386a4b6421f1
SettingDust <settingdust@gmail.com>
2024-01-12 01:03:09 +0800

    fix: TA effects will lose when create new style (#15)
    * fix: TA effects will lose when create new style
    
    * chore: add fabric loader greater than 0.15 for mixin extras
    
    * fix: don't apply effects to EMPTY
    
    * fix: remove rainbow for border

---
+++ src/main/java/snownee/textanimator/mixin/client/FontMixin.java
@@ -0,0 +1,33 @@
+package snownee.textanimator.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.google.common.collect.ImmutableList;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+
+import net.minecraft.client.gui.Font;
+import net.minecraft.network.chat.Style;
+import snownee.textanimator.duck.TAStyle;
+import snownee.textanimator.effect.RainbowEffect;
+
+@Mixin(Font.class)
+public class FontMixin {
+	@ModifyExpressionValue(
+			method = "method_37297",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/network/chat/Style;withColor(I)Lnet/minecraft/network/chat/Style;"
+			)
+	)
+	private Style textanimator$removeRainbowForBorder(Style original) {
+		final var style = (TAStyle) original;
+		if (style.textanimator$getEffects().isEmpty()) return original;
+		style.textanimator$setEffects(style.textanimator$getEffects()
+										   .stream()
+										   .filter(it -> !(it instanceof RainbowEffect))
+										   .collect(
+												   ImmutableList.toImmutableList()));
+		return original;
+	}
+}
