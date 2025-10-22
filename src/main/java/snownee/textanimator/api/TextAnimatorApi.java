8440293dec18e82ba3ca32a0bc9b98674ef9a906
QiHuang02 <128583144+QiHuang02@users.noreply.github.com>
2025-10-22 09:52:41 +0800

    feat: add API to temporarily disable rendering (#43)
    Co-authored-by: Snownee <i@snownee.com>

---
+++ src/main/java/snownee/textanimator/api/TextAnimatorApi.java
@@ -0,0 +1,35 @@
+package snownee.textanimator.api;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@SuppressWarnings("unused")
+public class TextAnimatorApi {
+	private static final AtomicInteger PARSING_SUSPEND_COUNT = new AtomicInteger();
+
+	public static AutoCloseable suspendParsing() {
+		PARSING_SUSPEND_COUNT.incrementAndGet();
+		return new AutoCloseable() {
+			private boolean closed;
+
+			@Override
+			public void close() {
+				if (!closed) {
+					closed = true;
+					resumeParsing();
+				}
+			}
+		};
+	}
+
+	public static void resumeParsing() {
+		PARSING_SUSPEND_COUNT.updateAndGet(value -> value > 0 ? value - 1 : 0);
+	}
+
+	public static boolean isParsingSuspended() {
+		return PARSING_SUSPEND_COUNT.get() > 0;
+	}
+
+	public static void resetParsingState() {
+		PARSING_SUSPEND_COUNT.set(0);
+	}
+}
--- src/main/java/snownee/textanimator/util/CommonProxy.java
+++ src/main/java/snownee/textanimator/util/CommonProxy.java
@@ -23,8 +23,10 @@ import net.minecraft.util.StringDecomposer;
 import snownee.textanimator.TextAnimator;
 import snownee.textanimator.TextAnimatorClient;
 import snownee.textanimator.TypewriterMode;
+import snownee.textanimator.api.TextAnimatorApi;
 import snownee.textanimator.duck.TAStyle;
 import snownee.textanimator.effect.Effect;
+import snownee.textanimator.effect.EffectFactory;
 import snownee.textanimator.effect.params.Params;
 import snownee.textanimator.mixin.StringDecomposerAccess;
 
@@ -104,7 +106,7 @@ public class CommonProxy implements ModInitializer {
 				++k;
 				continue;
 			}
-			if (c == '<') {
+			if (c == '<' && !TextAnimatorApi.isParsingSuspended()) {
 				StringBuilder sb = new StringBuilder();
 				for (int l = k + 1; l < j; ++l) {
 					char ch = string.charAt(l);
@@ -179,12 +181,61 @@ public class CommonProxy implements ModInitializer {
 		}
 	}
 
-	@Override
-	public void onInitialize() {
-		TextAnimator.init();
+	public static String stripEffectTags(String input) {
+		if (input == null || input.isEmpty()) {
+			return input;
+		}
+		int start = 0;
+		StringBuilder builder = null;
+		while (true) {
+			int open = input.indexOf('<', start);
+			if (open == -1) {
+				break;
+			}
+			int close = input.indexOf('>', open + 1);
+			if (close == -1) {
+				break;
+			}
+			String content = input.substring(open + 1, close);
+			boolean remove = false;
+			if (!content.isEmpty()) {
+				if (content.charAt(0) == '/') {
+					String type = content.substring(1);
+					remove = EffectFactory.listTypes().contains(type);
+				} else {
+					try {
+						Effect.create(content, true);
+						remove = true;
+					} catch (IllegalArgumentException ignored) {
+					}
+				}
+			}
+			if (remove) {
+				if (builder == null) {
+					builder = new StringBuilder(input.length());
+				}
+				builder.append(input, start, open);
+				start = close + 1;
+				continue;
+			}
+			if (builder != null) {
+				builder.append(input, start, close + 1);
+			}
+			start = close + 1;
+		}
+		if (builder == null) {
+			return input;
+		}
+		builder.append(input, start, input.length());
+		return builder.toString();
 	}
 
 	public static boolean isPhysicalClient() {
 		return FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT;
+	}
+
+	@Override
+	public void onInitialize() {
+		TextAnimator.init();
 	}
 }
